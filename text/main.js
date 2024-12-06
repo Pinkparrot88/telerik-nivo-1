a898af595d5a1cf6c3a3a13c31a430ff46785703
Pinkparrot88 <pinkparrot88@gmail.com>
2024-12-06 20:57:23 +0200

    added previous games

---
+++ text/main.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, create, update })
+
+
+
+function preload() {
+    
+}
+
+function create() {
+
+    const myStyle = {
+
+        font: '50px Tahoma',
+        fill: '#0eedbd',
+        backgroundColor: '#47b7d6',
+        align: 'center',
+        stroke: '#DC143C',
+        strokeThickness: 4
+    }
+    const text = game.add.text(game.width/2,game.height/2,"Slay", myStyle)
+    text.anchor.setTo(0.5)
+    text.scale.setTo(3)
+
+    const text2 = game.add.text(0,0,"Bukv 2.0", myStyle)
+
+    const text3 = game.add.text(game.width,game.height,"Bukv 3.0", myStyle)
+    text3.anchor.setTo(1)
+    
+}
+
+function update() {
+    
+    
+}
---
+++ timer/main.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, create, update })
+
+let x = 0
+let text
+function preload() {
+    
+}
+
+function create() {
+
+    const myStyle = {
+
+        font: '50px Tahoma',
+        fill: '#0eedbd',
+        backgroundColor: '#47b7d6',
+        align: 'center',
+        stroke: '#DC143C',
+        strokeThickness: 4
+    }
+
+    
+
+    text = game.add.text(game.width/2,game.height/2, x , myStyle)
+    text.anchor.setTo(0.5)
+    text.scale.setTo(3)
+
+
+}
+
+function update() {
+    x++
+    text.setText(x)
+  
+}
+
