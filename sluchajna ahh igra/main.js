bc98574175c0d800c4f52edcd7d164bfb12a9b3a
Pinkparrot88 <pinkparrot88@gmail.com>
2024-12-06 20:14:00 +0200

    Added index.html and formatted code

--- sluchajna ahh igra/main.js
+++ sluchajna ahh igra/main.js
@@ -3,34 +3,32 @@
 const game = new Phaser.Game(800, 800, Phaser.AUTO, 'game-canvas', { preload, create, update })
 
 function preload() {
-game.load.spritesheet("cat", "kitty run.jpg", 563/3, 375/2)
+  game.load.spritesheet("cat", "kitty run.jpg", 563 / 3, 375 / 2)
 
 }
 let cat
 let direction = 5
 function create() {
 
- 
 
-     cat = game.add.sprite(190,game.height,"cat")
-     cat.anchor.setTo(1)
-     cat.scale.setTo(1)
-     cat.animations.add("cat",[0,1,2,3,4,5,6],10,true)
-     cat.animations.play("cat")
- 
+
+  cat = game.add.sprite(190, game.height, "cat")
+  cat.anchor.setTo(1)
+  cat.scale.setTo(1)
+  cat.animations.add("cat", [0, 1, 2, 3, 4, 5, 6], 10, true)
+  cat.animations.play("cat")
+
 }
 
 function update() {
-    
-if (cat.x>game.width){
+
+  if (cat.x > game.width) {
     direction = -5
-}
+  }
 
-if (cat.x<190){
-  direction = 5
-}
-cat.x += direction
-   
-}
+  if (cat.x < 190) {
+    direction = 5
+  }
+  cat.x += direction
 
-a == 2
+}
